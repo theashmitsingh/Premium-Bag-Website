cd15c3feb6d324546ed2af28620e14c82da327c1
Ashmit Singh <theashmitsingh11@gmail.com>
2024-09-20 21:19:09 +0530

    Added separations of concerns in models and created owner model

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const db = require('./config/mongoose-connection');
+
+const ownersRouter = require('./routes/ownersRouter');
+const productsRouter = require('./routes/productsRouter');
+const usersRouter = require('./routes/usersRouter');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+
+app.get("/", (req, res, next) => {
+    res.send("Hello");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
+});
